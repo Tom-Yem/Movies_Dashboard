9ef326d49ef36c3fb37f51f5abe631b2a5c9ed03
Tom <Tomy51451@gmail.com>
2021-11-10 15:41:59 +0200

    :sparkles: NEW: Add drawer navigation

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+let drawerSurface = document.getElementById("drawerSurface");
+let drawer = document.getElementById("drawer");
+
 drawerOpener.onclick = () => {
-  console.log("hey");
+  let drawerSurface = document.getElementById("drawerSurface");
+  drawerSurface.style.display = "block";
 };
+
+drawerSurface.onclick = () => (drawerSurface.style.display = "none");
+
+//TODO: stop event propagation on drawer
+//TODO: make slidable animation
+//TODO: take care of drawer width on landscape view
+//TODO: check nav scrollability on drawer
